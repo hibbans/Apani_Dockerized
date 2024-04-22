7a5787e1c139da2493c4217476c30608eea0b1bb
hibbans <mhibbansyakir@gmail.com>
2024-04-22 11:03:42 +0700

    Commit Apani Docker Pertama

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
+router.get('/', function(req, res, next) {
+   res.status(200).render('index', { title: 'Express' });
+});
 
-router.get('/', (req, res) => {
+router.get('/index', (req, res) => {
     res.status(200).json({    
     message: 'ok' 
     });
